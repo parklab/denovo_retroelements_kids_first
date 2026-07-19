4b1ff3f09e8088fb847ba8e45d7ffef07a6fc748
Chong Chu <simon@Chongs-MacBook-Pro.local>
2026-07-18 22:51:23 -0400

    update readme and foldername

--- 1KGP_denovo_ins_inspection/gnrt_screenshot_with_bamsnap/run_pipeline.sh
+++
@@ -1,6 +0,0 @@
-bash fix_crlf_for_trio_bamsnap.sh
-rm -f 1000G_highcov_sample_map.tsv
-bash build_1000g_aws_sample_map.v2.sh $PWD
-bash validate_rare2_trio_samples.sh
-bash test_one_trio_bamsnap_rare2.v1.sh
-bash launch_trio_bamsnap_rare2.v1.sh
